d60eb554e9eb1ace264e92919c66edc9bd49405e
Diego Cortes <dc.diegocortes@gmail.com>
2026-04-28 12:40:05 +0200

    Add PHP exercise 1 in level02_1

---
+++ level02_1/exercise1.php
@@ -0,0 +1,17 @@
+<?php
+
+//variables + constants
+$width= 5;
+$price = 6.5;
+$userName = "pepa";
+$off = false;
+const OWNERNAME = "diego"; // const or define for constants 
+
+// output data w echo
+echo $width . "\n" .
+     $price . "\n" .
+     $userName . "\n" .
+     $off . "\n" .
+     OWNERNAME;
+
+?>
