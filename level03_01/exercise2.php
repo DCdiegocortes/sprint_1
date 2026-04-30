442b0f250d3d5da4f42bd5399eb9a9d695aabeff
Diego Cortes <dc.diegocortes@gmail.com>
2026-04-30 19:36:55 +0200

    Update level03_01 exercise2

--- level03_01/exercise2.php
+++ level03_01/exercise2.php
@@ -1,5 +1,18 @@
 <?php
 
+$gamesList = ["pokemon sword","super mario brothers","dragon ball ultimate",
+               "zelda breath of the wild"];
 
+echo count($gamesList) . "\n"; //array size
+
+unset($gamesList[1]); //delete element in index 1
+
+$gamesList = array_values($gamesList);//reindexar array
+
+echo count($gamesList) . "\n";
+
+foreach ($gamesList as $game) {
+    echo $game . "\n";
+}
 
 ?>
