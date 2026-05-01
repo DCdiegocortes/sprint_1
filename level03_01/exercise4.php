4ef59db66dba1d603fb5726dc5cdc137727c2d8e
Diego Cortes <dc.diegocortes@gmail.com>
2026-05-01 13:42:19 +0200

    add level03_01exercise4

---
+++ level03_01/exercise4.php
@@ -0,0 +1,14 @@
+<?php
+
+$myInfo = [
+    "name" => "lola",
+    "age" => 25,
+    "email" => "lolaperez@gmail.com",
+    "favoriteFood" => "ravioli"
+    ];
+
+foreach ($myInfo as $key => $value) {
+    echo $key . ": " . $value . "\n";
+}
+
+?>
