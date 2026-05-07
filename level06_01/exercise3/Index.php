80acbb3c478891bd370419656ba92762159408f9
Diego Cortes <dc.diegocortes@gmail.com>
2026-05-07 12:54:14 +0200

    add  level06_01/exercise3/Index.php level06_01/exercise3/magicMethods.php

---
+++ level06_01/exercise3/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "magicMethods.php";
+
+$bookList = [
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, 10.99),
+    new Book("To Kill a Mockingbird", "Harper Lee", 281, 7.99),
+    new Book("1984", "George Orwell", 328, 8.99)
+];
+
+foreach ($bookList as $currentBook) {
+    echo $currentBook . "\n"; // will call __TOSTRING method of the Book class
+}
+echo "----------------------------------------------------  \n";
+
+foreach ($bookList as $currentBook) {
+    echo "Title: " . $currentBook->title . ", Author: " . $currentBook->author . ", Pages: "
+                   . $currentBook->totalPages . ", Price: $" . $currentBook->price . "\n"; // calls __TOGET method of the Book class to access private propertie
+}
+
+?>
---
+++ level06_01/exercise3/magicMethods.php
@@ -0,0 +1,32 @@
+<?php
+
+class Book{
+
+    private $title;
+    private $author;
+    private $totalPages;
+    private $price;
+
+    public function __construct($title, $author, $totalPages, $price)
+    {
+        $this->title = $title;
+        $this->author = $author;
+        $this->totalPages = $totalPages;
+        $this->price = $price;
+    }
+
+    public function __get($property) // access to private or nonexistent properties
+    {
+        if(property_exists($this, $property)){
+            return $this->$property;
+        }
+    }
+
+    public function __toString() // object as text
+    {
+        return "Title: $this->title, Author: $this->author, Total Pages: $this->totalPages";
+    }
+
+}
+
+?>
