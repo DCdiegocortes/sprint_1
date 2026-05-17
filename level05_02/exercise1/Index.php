d31d86ede9c2945c283d15620e8f8cd9dbcb96d8
Diego Cortes <dc.diegocortes@gmail.com>
2026-05-17 21:21:30 +0200

    Add edge case tests for  level08_01/exercise1/NumberCheckerTest.php

--- level05_02/exercise1/Index.php
+++ level05_02/exercise1/Index.php
@@ -23,3 +23,4 @@ foreach ($shapes as $currentShape) {
 }
 
 ?>
+<
--- level08_01/exercise1/NumberCheckerTest.php
+++ level08_01/exercise1/NumberCheckerTest.php
@@ -5,23 +5,41 @@ require_once "NumberChecker.php";// include the NumberChecker class to be tested
 
 class NumberCheckerTest extends TestCase // create new class for testing + extends TestCase
 {
-    public function testIsPositive()
+    public function testZeroIsNotPositive()
     {
         $zeroNumberChecker = new NumberChecker(0);
         $this->assertFalse($zeroNumberChecker->isPositive());
     }
-    public function testIsEven()
+
+    public function testNumberIsNegative()
     {
-        $evenNumberChecker = new NumberChecker(4);
-        $this->assertTrue($evenNumberChecker->isEven()); // assertTrue = because 4 is even
+        $negativeNumberChecker = new NumberChecker(-5);
+        $this->assertFalse($negativeNumberChecker->isPositive());
     }
 
-    public function testIsNotEven()
+    public function testNumberIsPositive()
+    {
+        $positiveNumberChecker = new NumberChecker(1);
+        $this->assertTrue($positiveNumberChecker->isPositive());
+    }
+
+    public function testZeroIsEven()
+    {
+        $zeroNumberChecker = new NumberChecker(0);
+        $this->assertTrue($zeroNumberChecker->isEven());
+    }
+
+    public function testNumberIsEven()
+    {
+        $evenNumberChecker = new NumberChecker(4);
+        $this->assertTrue($evenNumberChecker->isEven());
+    }
+
+    public function testNumberIsNotEven()
     {
         $notEvenNumberChecker = new NumberChecker(5);
-        $this->assertFalse($notEvenNumberChecker->isEven()); // assertFalse = because 5 is not even
+        $this->assertFalse($notEvenNumberChecker->isEven());
     }
 
 }
-
 ?>
